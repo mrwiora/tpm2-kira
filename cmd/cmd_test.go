1d9772b96609553d797fb1cae846a99210538308
Matthias Robert Wiora <matthias@wiora.io>
2026-02-08 17:24:08 +0100

    fix(fmt): fix format

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -758,8 +758,8 @@ func TestUnmarshalSealedBlob_OversizedFields(t *testing.T) {
 			blobMaker: func() []byte {
 				blob := make([]byte, 16)
 				binary.LittleEndian.PutUint32(blob[0:4], CurrentBlobVersion) // version
-				binary.LittleEndian.PutUint32(blob[4:8], 0)                 // appVersionLen=0
-				binary.LittleEndian.PutUint32(blob[8:12], MaxPublicLen+1)   // too large
+				binary.LittleEndian.PutUint32(blob[4:8], 0)                  // appVersionLen=0
+				binary.LittleEndian.PutUint32(blob[8:12], MaxPublicLen+1)    // too large
 				binary.LittleEndian.PutUint32(blob[12:16], 0)
 				return blob
 			},
@@ -770,8 +770,8 @@ func TestUnmarshalSealedBlob_OversizedFields(t *testing.T) {
 			blobMaker: func() []byte {
 				blob := make([]byte, 20)
 				binary.LittleEndian.PutUint32(blob[0:4], CurrentBlobVersion) // version
-				binary.LittleEndian.PutUint32(blob[4:8], 0)                 // appVersionLen=0
-				binary.LittleEndian.PutUint32(blob[8:12], 0)                // publicLen=0
+				binary.LittleEndian.PutUint32(blob[4:8], 0)                  // appVersionLen=0
+				binary.LittleEndian.PutUint32(blob[8:12], 0)                 // publicLen=0
 				binary.LittleEndian.PutUint32(blob[12:16], MaxPrivateLen+1)  // too large
 				binary.LittleEndian.PutUint32(blob[16:20], 0)
 				return blob
